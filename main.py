e32d1f2bec8cc1784832678f769eae2126d28c22
= <zabivayvsegda@mail.com>
2023-09-13 14:02:04 +0300

    Container item

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import aiogram
-from config import TOKEN
+from config import *
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types.web_app_info import WebAppInfo
 
@@ -9,7 +9,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Начать', web_app=WebAppInfo(url="https://www.google.com/")))
+    markup.add(types.KeyboardButton('Начать', web_app=WebAppInfo(url=WEBAPP_URL)))
     await message.answer("Добро пожаловать!", reply_markup=markup)
 
 
